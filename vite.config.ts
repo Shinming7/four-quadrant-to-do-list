f0dd715ec85f2724b170b8158ab1a328682fddf5
shinming <Shinming0104@163.com>
2026-08-20 15:16:11 +0800

    1.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteSingleFile(),
+    {
+      name: 'remove-single-file-module-attribute',
+      enforce: 'post',
+      transformIndexHtml(html) {
+        return html.replace('<script type="module" crossorigin>', '<script>')
+      },
+    },
+  ],
 })
