1e16d9e9a7a18c4827b96f6d4a475d6914e58acd
shinming <Shinming0104@163.com>
2026-08-20 15:39:00 +0800

    fix GitHub Pages runtime build

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: './',
-  plugins: [
-    react(),
-    viteSingleFile(),
-    {
-      name: 'remove-single-file-module-attribute',
-      enforce: 'post',
-      transformIndexHtml(html) {
-        return html.replace('<script type="module" crossorigin>', '<script>')
-      },
-    },
-  ],
+  plugins: [react()],
 })
